b7302dc0a2f2166732d7568c9ef3003a736dbebf
benjaaracil <benjaminaracil@gmail.com>
2021-11-10 14:04:45 -0300

    Emprolijando home

--- src/components/Ciudad.jsx
+++ src/components/Ciudad.jsx
@@ -8,7 +8,7 @@ export default function Ciudad({city}) {
             <div className="ciudadCard">
                     <h2 className = "titulo">{city.name}</h2>
                     <div>
-                        <div>Temperature: {city.temp} ºC</div>
+                        <div>Temperature: {city.temp}°C</div>
                         <div>Weather: {city.weather}</div>
                         <div>Wind: {city.wind} km/h</div>
                         <div>Clouds: {city.clouds}</div>
